b81db7b7cc7a9f42dc5f7b89226a791370096c10
Thomas Kolonko <thomas.kolonko@ilub.unibe.ch>
2018-12-04 13:08:19 +0100

    updated for ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'xoct';
 $version = '1.21';
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'support@studer-raimann.ch';
