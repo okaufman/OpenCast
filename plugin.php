4a251d2fc5870e2deb7f7be615402fb418774589
root <root@ilias-webng2.unibe.ch>
2021-06-08 13:56:27 +0200

    update for ilias7

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'xoct';
 $version = '3.1.1';
 $ilias_min_version = '5.4.0';
-$ilias_max_version = '6.999';
+$ilias_max_version = '7.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'support@studer-raimann.ch';
