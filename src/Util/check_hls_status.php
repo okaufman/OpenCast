d39e3c1d9608729f488b8d3c8caef4c0ee6c8f20
okaufman <olivia.kaufmann@unibe.ch>
2022-07-12 14:10:12 +0200

    fix Livestream for update

--- src/Util/check_hls_status.php
+++ src/Util/check_hls_status.php
@@ -35,7 +35,7 @@ function parsePlaylist(string $m3u8) : array
     $pieces = explode("\n", $m3u8); // make an array out of curl return value
     $pieces = array_map('trim', $pieces); // remove unnecessary space
     $chunklists = array_filter($pieces, function (string $piece) { // pluck out m3u8 urls
-       return strtolower(substr($piece, -5)) === '.m3u8';
+       return strtolower(substr($piece, -3)) === '.ts';
     });
     return $chunklists;
 }
@@ -45,7 +45,7 @@ $response = fetch($url);
 $url = $response['effective_url'] ?? $url;
 $base_url = substr($url, 0, strrpos($url, '/') + 1);
 // check playlist
-if (($response['httpCode'] !== 200) || (strpos($response['body'], 'EXT-X-STREAM-INF') === false)) {
+if (($response['httpCode'] !== 200) || (strpos($response['body'], 'EXT-X-MEDIA-SEQUENCE') === false)) {
     echo 'false';
     exit;
 }
